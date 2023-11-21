bbd7eb0afa53fa30ea6a9632916960fc2da62e39
Manoj Paudel <109033830+sudomanoj@users.noreply.github.com>
2023-11-21 20:34:46 +0545

    Update Question1

--- Question1.py
+++ Question1.py
@@ -1,3 +1,5 @@
+1. Write a program that takes a number as input from the user and prints all the even numbers up to that number using a loop and conditional statement.
+
 num = int(input('Enter a number: '))
 for i in range(num + 1):
     if i % 2 == 0:
