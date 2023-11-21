1462e54227f57b7f64e37e767fc3f96ca37dce0a
Manoj Paudel <109033830+sudomanoj@users.noreply.github.com>
2023-11-21 20:33:32 +0545

    Solved Question1.py

---
+++ Question1.py
@@ -0,0 +1,7 @@
+num = int(input('Enter a number: '))
+for i in range(num + 1):
+    if i % 2 == 0:
+        print(i)
+import random
+secret_num = random.randint(0, 100)
+number = int(input('Enter a number between 1 and 100: '))
