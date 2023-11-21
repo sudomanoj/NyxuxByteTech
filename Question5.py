8b77c1e2fbbe8dc2d5f23eac42f9107a7d872f7b
Manoj Paudel <109033830+sudomanoj@users.noreply.github.com>
2023-11-21 21:25:46 +0545

    Created Question5

---
+++ Question5.py
@@ -0,0 +1,15 @@
+num1 = int(input('Enter a first number: '))
+num2 = int(input('Enter a second number: '))
+op = input('Enter a operator: ')
+
+if op == '+':
+    print(num1+num2)
+
+elif op == '-':
+    print(num1-num2)
+    
+elif op == '*':
+    print(num1*num2)
+    
+if op == '/':
+    print(num1/num2)
