d85ff8df738784b2be1da1752f17edb1ce0af27c
Manoj Paudel <109033830+sudomanoj@users.noreply.github.com>
2023-11-22 18:49:01 +0545

    Updated question3

--- Day1_task2/question3.py
+++ Day1_task2/question3.py
@@ -9,19 +9,24 @@ def car_racing():
     while play:
         new_state = input('Enter the new state of car: ')
         new_state = new_state.lower()
-        if(new_state == initial_state):
-            print(f'Car is already in {initial_state} state')
-        elif(new_state == 'exit'):
-            are_you_sure = input('Are you sure to exit the game?(yes/no)').lower()
-            if are_you_sure == 'yes':
-                play = False
+        states = ['start', 'stop', 'exit']
+        if new_state in states:
+            if(new_state == initial_state):
+                print(f'Car is already in {initial_state} state')
+            elif(new_state == 'exit'):
+                are_you_sure = input('Are you sure to exit the game?(yes/no)').lower()
+                if are_you_sure == 'yes':
+                    play = False
+                    
+            elif(new_state != initial_state or new_state != 'exit'):
+                initial_state = new_state
+                print(f'Car shifted to {initial_state}')
                 
-        elif(new_state != initial_state or new_state != 'exit'):
-            initial_state = new_state
-            print(f'Car shifted to {initial_state}')
-            
+            else:
+                print('Enter a valid input!')
         else:
-            print('Enter a valid input!')
+            print('Invalid Input!!!')
+            print(hint)
 
 car_racing()
             
